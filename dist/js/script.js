b5ad26b1c738cec34780a2d15aebc67ff906f1fe
jovahn <jovanshann@gmail.com>
2024-05-14 16:55:03 +0700

    Day 2 Skills Section

--- dist/js/script.js
+++ dist/js/script.js
@@ -10,6 +10,17 @@ cta.forEach(el => {
     });
 });
 
+const cards = document.querySelectorAll('.card');
+cards.forEach(el => {
+    el.onmousehover = function (el) {
+        const x = el.pageX - el.offsetLeft;
+        const y = el.pageY - el.offsetTop;
+
+        el.style.setProperty('--x', x + 'px');
+        el.style.setProperty('--y', y + 'px');
+    }
+});
+
 
 // cta.addEventListener("click", (e) => {
 //     e.preventDefault;
--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,7 @@ module.exports = {
       },
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'],
-      },
+      }
     }
   },
   plugins: [],
