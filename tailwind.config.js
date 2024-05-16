bfc0c4af4a6e73149858bd81e8986a9dee44db04
jovahnsc <jovanshann@gmail.com>
2024-05-17 00:17:09 +0700

    Day 4 Skill Content Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,40 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}", "./index.html"],
-  darkMode: 'selector',
+  darkMode: "selector",
   theme: {
     extend: {
       colors: {
-        bgColor: '#081b29',
-        secondBgColor: '#112e42',
-        textColor: '#ededed',
-        mainColor: '#00abf0',
+        bgColor: "#081b29",
+        secondBgColor: "#112e42",
+        textColor: "#ededed",
+        mainColor: "#00abf0",
       },
-      backgroundImage: {
-        'image-radial': 'radial-gradient(circle, #7d2ae8 20%, transparent 20%)',
-      },
-      backgroundSize: {
-        'size-radial': '10% 10%',
-      },
+      // backgroundImage: {
+      //   "image-radial": "radial-gradient(circle, #7d2ae8 20%, transparent 20%)",
+      // },
+      // backgroundSize: {
+      //   "size-radial": "10% 10%",
+      // },
       fontFamily: {
-        poppins: ['Poppins', 'sans-serif'],
+        poppins: ["Poppins", "sans-serif"],
       },
       animation: {
-        imgFloat: 'imgFloat 5s ease-in-out infinite',
+        imgFloat: "imgFloat 5s ease-in-out infinite",
       },
       keyframes: {
-        wiggle: {
-          '0%, 100%': { transform: 'rotate(-3deg)', backgroundsize: '10%' },
-          '50%': { transform: 'rotate(3deg)' },
-        },
         imgFloat: {
-          '50%': {
-            transform: 'translateY(10px)',
-            borderRadius: '40% 65% 40% 65%'
+          "50%": {
+            transform: "translateY(10px)",
+            borderRadius: "40% 65% 40% 65%",
           },
         },
-      }
-    }
+      },
+    },
   },
   plugins: [],
 };
